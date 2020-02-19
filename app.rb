e1031e6299fa3ca4733c380d0a2f7ee432564483
Antony Sanders <antony.devon92@gmail.com>
2020-02-19 19:16:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,16 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-
+  get '/newteam' do
+    erb :newteam
+  end 
+  
+  get '/team' do
+    # @bball_team = Team.new(params[:name], params[:coach], params[:pg], params[:sg], params[:sf], params[:pf], params[:c])
+    
+    erb :team
+    
+  end 
+  
+    
 end
